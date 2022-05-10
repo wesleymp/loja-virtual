c757a943f20eb89738009e6407b5939afb2d13c6
wesleymp <wesleyxd@hotmail.com>
2022-05-10 12:52:10 -0300

    feat: adicionado o proccess on uncaughtExecption para evitar que aplicação pare com erros inesperados

--- src/main/server.js
+++ src/main/server.js
@@ -2,3 +2,5 @@ const app = require('./app');
 require('dotenv').config();
 
 app.listen(process.env.PORT, () => console.log('Server is running'));
+
+process.on('uncaughtException', (_error, _origin) => { });
