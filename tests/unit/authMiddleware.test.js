4c1a2dc9906554e4fddc6ad75faec12c94743e0b
wesleymp <wesleyxd@hotmail.com>
2022-05-10 02:48:54 -0300

    test: criado o teste de unidade para o middleware authMiddleware

---
+++ tests/unit/authMiddleware.test.js
@@ -0,0 +1,57 @@
+const sinon = require('sinon');
+const { authMiddleware } = require('../../src/middlewares');
+const { genereteJwt } = require('../../src/util/jwt');
+
+describe('Middleware authMiddleware', () => {
+  const req = {};
+  const res = {};
+  const next = sinon.stub();
+
+  const data = {
+    id: 2,
+    id_role: 2,
+  };
+
+  const token = genereteJwt(data);
+
+  beforeEach(() => {
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
+
+  it('deve retornar um status 401 quando não existir o cabeçalho authorization', () => {
+    req.headers = {};
+    authMiddleware(req, res, next);
+    expect(res.status.calledWith(401)).toBe(true);
+  });
+
+  it('deve retornar uma mensagem { message: "Token não encontrado." } quando não existir o cabeçalho authorization', () => {
+    req.headers = {};
+    authMiddleware(req, res, next);
+    expect(res.json.calledWith({ message: 'Token não encontrado.' })).toBe(true);
+  });
+
+  it('deve retornar um status 401 quando for enviado um token inválido', () => {
+    req.headers = {
+      authorization: 'invalid_token',
+    };
+    authMiddleware(req, res, next);
+    expect(res.status.calledWith(401)).toBe(true);
+  });
+
+  it('deve retornar uma mensagem { message: "Token inválido." } quando for enviado um token inválido', () => {
+    req.headers = {
+      authorization: 'invalid_token',
+    };
+    authMiddleware(req, res, next);
+    expect(res.json.calledWith({ message: 'Token inválido.' })).toBe(true);
+  });
+
+  it('deve retornar a função next caso o token informado estiver correto', () => {
+    req.headers = {
+      authorization: token,
+    };
+    authMiddleware(req, res, next);
+    sinon.assert.calledOnce(next);
+  });
+});
