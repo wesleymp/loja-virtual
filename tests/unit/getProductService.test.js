c77621a7a8f78e7d2d5a4dcc0bc891174d946f2a
wesleymp <wesleyxd@hotmail.com>
2022-05-10 14:49:45 -0300

    test: criado o teste de unidade do service getProductService

---
+++ tests/unit/getProductService.test.js
@@ -0,0 +1,41 @@
+const sinon = require('sinon');
+const services = require('../../src/services');
+const models = require('../../src/models');
+const products = require('../memory-data/products');
+require('dotenv').config();
+
+describe('Service getProductService', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('deve retornar um status code 200 caso existir produtos cadastrados', async () => {
+    sinon.stub(models, 'getProductModel').resolves({ rowCount: 3, rows: products });
+    const dataRegister = await services.getProductService();
+    expect(dataRegister.status).toBe(200);
+  });
+
+  it('deve retornar um json com os dados do produto caso existir produtos cadastrados', async () => {
+    sinon.stub(models, 'getProductModel').resolves({ rowCount: 3, rows: products });
+    const dataRegister = await services.getProductService();
+    expect(dataRegister.data).toMatchObject(products);
+  });
+
+  it('deve retornar um status code 404 caso não existir produtos cadastrados', async () => {
+    sinon.stub(models, 'getProductModel').resolves({ rowCount: 0, rows: [] });
+    try {
+      await services.getProductService();
+    } catch (error) {
+      expect(error.status).toBe(404);
+    }
+  });
+
+  it('deve retornar uma mensagem { message: "Nenhum produto cadastrado" } caso não existir produtos cadastrados', async () => {
+    sinon.stub(models, 'getProductModel').resolves({ rowCount: 0, rows: [] });
+    try {
+      await services.getProductService();
+    } catch (error) {
+      expect(error).toMatchObject({ message: 'Nenhum produto cadastrado' });
+    }
+  });
+});
