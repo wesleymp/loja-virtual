a887dfcf0323abb40f155fb3bb63f9b0558e44d2
wesleymp <wesleyxd@hotmail.com>
2022-05-10 14:04:29 -0300

    test: criado o teste de unidade do service getManagementService

---
+++ tests/unit/getManagementService.test.js
@@ -0,0 +1,41 @@
+const sinon = require('sinon');
+const services = require('../../src/services');
+const models = require('../../src/models');
+const users = require('../memory-data/users');
+require('dotenv').config();
+
+describe('Service getManagementService', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('deve retornar um status code 200 caso existir usuários cadastrados', async () => {
+    sinon.stub(models, 'getManagementModel').resolves({ rowCount: 2, rows: users });
+    const dataRegister = await services.getManagementService();
+    expect(dataRegister.status).toBe(200);
+  });
+
+  it('deve retornar um json com os dados do usuário caso existir usuários cadastrados', async () => {
+    sinon.stub(models, 'getManagementModel').resolves({ rowCount: 2, rows: users });
+    const dataRegister = await services.getManagementService();
+    expect(dataRegister.data).toMatchObject(users);
+  });
+
+  it('deve retornar um status code 404 caso não existir usuários cadastrados', async () => {
+    sinon.stub(models, 'getManagementModel').resolves({ rowCount: 0, rows: [] });
+    try {
+      await services.getManagementService();
+    } catch (error) {
+      expect(error.status).toBe(404);
+    }
+  });
+
+  it('deve retornar uma mensagem { message: "Nenhum usuário cadastrado" } caso não existir usuários cadastrados', async () => {
+    sinon.stub(models, 'getManagementModel').resolves({ rowCount: 0, rows: [] });
+    try {
+      await services.getManagementService();
+    } catch (error) {
+      expect(error).toMatchObject({ message: 'Nenhum usuário cadastrado' });
+    }
+  });
+});
