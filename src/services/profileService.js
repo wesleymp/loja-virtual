ea813c47a30215e3307ee82eadddc5456a193896
wesleymp <wesleyxd@hotmail.com>
2022-05-11 18:51:20 -0300

    fix: ajustando token jwt e add cluster no pm2

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -3,7 +3,7 @@ const { verifyToken } = require('../util/jwt');
 
 const profileService = async (authorization) => {
   const data = verifyToken(authorization);
-  const { rows } = await models.profileModel(data.id);
+  const { rows } = await models.profileModel(data.id_user);
   delete rows[0].password;
   return { status: 200, data: rows[0] };
 };
