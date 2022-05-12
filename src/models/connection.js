00611273bc3f89e38bf2c26a490afe435e4678d4
wesleymp <wesleyxd@hotmail.com>
2022-05-12 19:46:02 -0300

    fix: add ssl rejectUnauthorized  na conexão

--- src/models/connection.js
+++ src/models/connection.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
   pool: { min: 0, max: 10 },
+  ssl: { rejectUnauthorized: false },
 });
 
 connection.on('error', (err) => {
